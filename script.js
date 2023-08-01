6a7b7715a1d8f8283fb2e8fb593d617b16d17f96
colinmoorecodes <moorecolinart@gmail.com>
2023-07-31 18:18:48 -0700

    Finished

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 let numBoxes = 16
-
+const width = 960
 const container = document.querySelector('.container')
-for (let i = 0; i < numBoxes; i++ ) {
-    for (let j = 0; j < numBoxes; j++) {
-        let div = document.createElement('div')
-        container.appendChild(div)
+
+function makeBoxes() {
+    let boxWidth = Math.floor(width/numBoxes)
+    for (let i = 0; i < numBoxes; i++ ) {
+        for (let j = 0; j < numBoxes; j++) {
+            let div = document.createElement('div')
+            container.appendChild(div)
+            div.style.width = boxWidth + "px"
+            div.style.height = boxWidth + "px"
+            div.addEventListener("mouseover", ()=> {
+                div.classList.add("dark")
+            })
+        }
     }
 }
+
+function changeSize() {
+    numBoxes = prompt("Enter a size of 100 or less")
+    if (isNaN(numBoxes) || numBoxes > 100 || numBoxes < 1) {
+        changeSize()
+    } else {
+        container.innerHTML = ""
+        makeBoxes()
+    }
+}
+
+makeBoxes()
